b7636cba3f02457d523f9b9f08fa6eca390553d3
Orlando Diaz <orlandodiazconde@gmail.com>
2023-04-04 08:56:35 -0500

    Resolve 1.1 and 1.2

--- part1/intro/src/App.jsx
+++ part1/intro/src/App.jsx
@@ -9,19 +9,35 @@ function App() {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
+}
+
+
+
+function Header({course}) {
+  return <h1>{course}</h1>
+}
+
+function Part({part, exercises}) {
+  return <p>{part} {exercises}</p>
+}
+
+function Content({part1, exercises1, part2, exercises2, part3, exercises3}){
+  return (
+  <>
+    <Part part={part1} exercises={exercises1}/>
+    <Part part={part2} exercises={exercises2}/>
+    <Part part={part3} exercises={exercises3}/>
+  </>
+  )
+}
+
+function Total({exercises1, exercises2, exercises3}) {
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 }
 
 export default App
